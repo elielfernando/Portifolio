7a80d5e55404a6b39315a55b6a6ef5990be644f6
ELiel Fernando <elielfernando851@gmail.com>
2026-04-09 23:16:13 -0300

    ajuste nas rotas

---
+++ portfolio-eliel/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MenuComponent, FooterComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('portfolio-renato');
+}
